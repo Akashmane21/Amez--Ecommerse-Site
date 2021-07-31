c5ef9dd34d8cf7e039c8fbf1ff5af7d2498eee76
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2021-07-31 17:39:59 +0530

    Slider img changed

--- src/Amazon/Component/Upper.jsx
+++ src/Amazon/Component/Upper.jsx
@@ -11,24 +11,24 @@ return (
 <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
 <div class="carousel-inner">
 
-<div class="carousel-item active upper" data-interval="10000">
-<img src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/PC/Mayactivation/Accessoriesday1/D23140543_IN_CEPC_Electronicsaccessories_underRs999_3000x12000.5x._CB669031984_.jpg" class="d-block w-100" alt="..."  />
+<div class="carousel-item active " data-interval="10000">
+<img src="https://neilpatel.com/wp-content/uploads/2018/05/pasted-image-0-320.png" class="d-block w-100" alt="..."  />
 
 </div>
 <div class="carousel-item" data-interval="2000">
-<img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/katariy/GW/BAU/Nord_2/D25332059_WLD_Nord_Dennis-Launch_Tall_Hero_1500x600._CB644910408_.jpg" class="d-block w-100" alt="..." />
+<img src="https://c4.wallpaperflare.com/wallpaper/153/263/761/advertising-art-digital-logo-wallpaper-preview.jpg" class="d-block w-100" alt="..." />
 
 </div>
 <div class="carousel-item" data-interval="2000">
-<img src="https://images-eu.ssl-images-amazon.com/images/G/31/img16/AmazonPayTravels/FlightsHero/revised/GCG_Flight_Herotator_desktop_1500x600_Avataar._CB664434600_.jpg" class="d-block w-100" alt="..." />
+<img src="https://www.designyourway.net/blog/wp-content/uploads/2010/11/Nike-Print-Ads-8.jpg" class="d-block w-100" alt="..." />
 
 </div>
 <div class="carousel-item" data-interval="2000">
-<img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Gateway_JWELSSH/Feb/SSW/Super_saver_brands/1500PC._CB658944950_.jpg" class="d-block w-100" alt="..." />
+<img src="https://i.pinimg.com/originals/c3/df/f8/c3dff88b7129438af19b7e759a97b119.jpg" class="d-block w-100" alt="..." />
 
 </div>
 <div class="carousel-item">
-<img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Audio/MegaMusicFest/July21/Musicfest_Gw_1500x600._CB645098031_.jpg" class="d-block w-100" alt="..." />
+<img src="https://www.myimaginestore.com/images/series-6/series-6-1.jpg" class="d-block w-100" alt="..." />
 
 </div>
 
