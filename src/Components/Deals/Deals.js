b7d6fb8184f6aac78ef8c0908fe09d4c5caa7650
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2021-08-04 21:06:20 +0530

    Menu Fixedej

--- src/Components/Deals/Deals.js
+++ src/Components/Deals/Deals.js
@@ -32,7 +32,7 @@ function Deals({Product}) {
         Cartdata.push(item)
         var string = JSON.stringify(Cartdata);
         localStorage.setItem('cartData',string);
-        
+       
     }
 
 
@@ -55,13 +55,15 @@ function Deals({Product}) {
             </div>
 
                 <div className="cart_btn">
-                <button onClick={Add}>
+               <a href="/">
+
+              <button onClick={Add}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
 
 Add to Cart
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="orange" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><circle cx="10" cy="20.5" r="1"/><circle cx="18" cy="20.5" r="1"/><path d="M2.5 2.5h3l2.7 12.4a2 2 0 0 0 2 1.6h7.7a2 2 0 0 0 2-1.6l1.6-8.4H7.1"/></svg>
 </button>
-
+ </a> 
                 </div>
 
 
