8fb2206d779a8040222887941e0ff2d442aa7054
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2021-08-07 18:48:40 +0530

    detail page addedhjjjk

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -66,7 +66,7 @@ function Detail() {
 
 
                 <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="carousel-control-prev-icon bg-gray" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
                 </a>
                 <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
@@ -128,7 +128,11 @@ function Detail() {
 
               
 
-          
+          <div className="shop_btns">
+              <button className="buy_btn">Buy Now <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="green" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 2L3 6v14c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2V6l-3-4H6zM3.8 6h16.4M16 10a4 4 0 1 1-8 0"/></svg></button>
+              <button className="cart_btn">Add to Cart <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="orange" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="10" cy="20.5" r="1"/><circle cx="18" cy="20.5" r="1"/><path d="M2.5 2.5h3l2.7 12.4a2 2 0 0 0 2 1.6h7.7a2 2 0 0 0 2-1.6l1.6-8.4H7.1"/></svg></button>
+
+          </div>
 
 
             </div>
