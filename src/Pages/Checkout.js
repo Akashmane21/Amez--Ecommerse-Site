7367b68135c06f555af0bab31adb8c06881b8f28
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2021-08-08 21:01:08 +0530

    place order

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -2,10 +2,12 @@ import '../CSS/Checkout.scss'
 import Header from '../header/Header';
 import {useEffect} from 'react'
 import {NavLink} from 'react-router-dom'
+import {useCounter} from '../Context/CartContext'
 
 
 
 function Checkout() {
+  const {Desdata } = useCounter()
 
 
   useEffect(() => {
@@ -26,7 +28,7 @@ function Checkout() {
      <div className="Packblock">
        <div className="Packleft_area">
 
-              <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_h439ffuh.json"  background="transparent"  speed="1"   loop  autoplay></lottie-player>
+              <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_99VKMQ.json"  background="transparent"  speed="1"   loop  autoplay></lottie-player>
 
             
   
@@ -40,15 +42,43 @@ function Checkout() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z"/><circle cx="12" cy="10" r="3"/></svg>
             </div>
 
+
+            <div className="data">
+            <div>
             <h2>Name :</h2>
-            <input placeholder="Ener your Name here" required />
+              <input placeholder="Ener your Name here" required />
 
+            </div>
+
+            <div>
             <h2>Phone Number :</h2>
-            <input placeholder="Enter Phonr Number" required/>
+              <input placeholder="Enter Phonr Number" required/>
 
+            </div>
+
+            </div>
+            
+           
             <h2>Address :</h2>
-            <textarea rows={4} placeholder="Enter your Address here with pin-code" />
+            <textarea rows={3} placeholder="Enter your Address here with pin-code" />
             <br />
+
+            <div className="product">
+
+            <div>
+               <h4>{Desdata.Name}</h4>
+              <h4 className="price">₹{Desdata.Price}</h4>
+            </div>
+
+            <img src={Desdata.Image1} alt="" />
+             
+
+            </div>
+
+
+
+
+
             <NavLink exact activeClassName = "active_class" to="/">
 
             <button className="cancel">Cancel 
--- src/header/Header.js
+++ src/header/Header.js
@@ -121,8 +121,7 @@ useEffect(() => {
 
               {/* <span className="cartlen">1</span> */}
 
-              {/* <svg xmlns="http://www.w3.org/2000/svg" width="32" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><path d="M22 17H2a3 3 0 0 0 3-3V9a7 7 0 0 1 14 0v5a3 3 0 0 0 3 3zm-8.27 4a2 2 0 0 1-3.46 0"></path></svg> */}
-              <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 17l5-5-5-5"/><path d="M13.8 12H3m9 10a10 10 0 1 0 0-20"/></svg>
+         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11.5" cy="8.5" r="5.5"/><path d="M11.5 14v7"/></svg>
               <h5>Orders</h5>
               </div>
               </a>
