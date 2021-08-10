96442e1c71e3643e1ad0266c090736f3ddce3d21
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2021-08-10 20:56:03 +0530

    lfavkhrer

--- src/Pages/Orders.js
+++ src/Pages/Orders.js
@@ -79,9 +79,9 @@ var AllOrdersdata = []
 
 
        <div className="right_area  h11">
-        <h1>Your Orders ___
+        <h6>Your Orders ___
         <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="10" r="3"/><path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z"/></svg>
-        </h1>
+        </h6>
 
 <div className="Ordered_items">
 
