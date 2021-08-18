0d157cf6901b14f9d2d84a28bc1c6242277c7ae7
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2021-08-18 12:38:30 +0530

    Dashboard Completedd

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -133,7 +133,10 @@ function AddFeature(){
         <button onClick={Logout}>Logout_<i class="fa fa-sign-out" aria-hidden="true"></i></button>
         </div>
         <h1 className="" >Hello {UserName} 👋</h1>
+<h1>
+Your Shop is Ready          <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="orangered" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
 <hr />
+           </h1>
 
 <div className="boxes">
     <div className="box" onClick={()=> setisAdd(!isAdd) }>
